443eedd6b17bfa78a00e4ec42873acdaec26b928
Phil Miller <phillip.miller@digital.cabinet-office.gov.uk>
2020-07-29 11:25:41 +0100

    Create a new generation of dev-pki certs
    Creates a generation of root, intermediary and leaf certs - tagged as generation "A1".
    Files and common names have also been renamed to make them distinct from existing PKI material.

--- scripts/generate-certs.sh
+++ scripts/generate-certs.sh
@@ -21,25 +21,24 @@ rm -rf $PKI_DIR && mkdir -p $CA_CERTS_DIR $DEV_KEYS_DIR
 # Create the CA certs
 cd $CA_CERTS_DIR
 
-sed 's/$COMMON_NAME/IDAP Dev Root CA/' $CSR_TEMPLATE | cfssl genkey -initca /dev/stdin | cfssljson -bare ida-root-ca
+sed 's/$COMMON_NAME/GOV.UK Verify Test Root CA/' $CSR_TEMPLATE | cfssl genkey -initca /dev/stdin | cfssljson -bare verify-root-ca
 
 function createInterCa {
 	name=$1
 	commonName=$2
 	sed "s/\$COMMON_NAME/$commonName/" $CSR_TEMPLATE |
-	cfssl gencert -config $CFSSL_CONFIG -profile intermediate -ca ./ida-root-ca.pem -ca-key ./ida-root-ca-key.pem /dev/stdin |
+	cfssl gencert -config $CFSSL_CONFIG -profile intermediate -ca ./verify-root-ca.pem -ca-key ./verify-root-ca-key.pem /dev/stdin |
 	cfssljson -bare $name
 }
-createInterCa idap-core-ca 'IDAP Core CA Dev'
-createInterCa ida-intermediary-ca 'IDA Inter CA Dev'
-createInterCa ida-intermediary-rp-ca 'IDA Inter RP CA Dev'
-createInterCa ida-metadata-ca 'IDA Metadata CA Dev'
+createInterCa verify-core-ca 'GOV.UK Verify Test Core CA'
+createInterCa verify-intermediary-ca 'GOV.UK Verify Test Intermediate CA'
+createInterCa verify-intermediary-rp-ca 'GOV.UK Verify Test Intermediate RP CA'
+createInterCa verify-metadata-ca 'GOV.UK Verify Test Metadata CA'
 
 # Add .test to all of the generated pems (to match the old file names)
 for file in *.pem; do mv $file $file.test; done
 
 # Generate leaf certs and keys
-
 cd $DEV_KEYS_DIR
 
 sed 's/$COMMON_NAME/Expired Test/' $CSR_TEMPLATE | cfssl selfsign -config $CFSSL_CONFIG -profile signing_expired dev.signin.service.gov.uk /dev/stdin | cfssljson -bare expired_self_signed_signing
@@ -53,30 +52,30 @@ function createLeaf {
 	cfssl gencert -config $CFSSL_CONFIG -profile $profile -ca $CA_CERTS_DIR/$ca.pem.test -ca-key $CA_CERTS_DIR/$ca-key.pem.test /dev/stdin |
 	cfssljson -bare $name
 }
-createLeaf metadata_signing_a                 		 ida-metadata-ca        signing            			'IDA Metadata Signing Dev A'
-createLeaf metadata_signing_b                 		 ida-metadata-ca        signing            			'IDA Metadata Signing Dev B'
-createLeaf hub_signing_primary                		 idap-core-ca           signing            			'IDA Hub Signing Dev'
-createLeaf hub_encryption_primary             		 idap-core-ca           encipherment       			'IDA Hub Encryption Dev'
-createLeaf hub_connector_signing_primary      		 idap-core-ca           signing            			'IDA Connector Hub Signing Dev'
-createLeaf hub_connector_encryption_primary   		 idap-core-ca           encipherment       			'IDA Connector Hub Encryption Dev'
-createLeaf sample_rp_encryption_primary       		 ida-intermediary-rp-ca encipherment       			'IDA Sample RP Encryption Dev'
-createLeaf sample_rp_msa_encryption_primary   		 ida-intermediary-rp-ca encipherment       			'IDA Sample RP MSA Encryption Dev'
-createLeaf sample_rp_msa_signing_primary      		 ida-intermediary-rp-ca signing            			'IDA Sample RP MSA Signing Dev'
-createLeaf sample_rp_signing_primary          		 ida-intermediary-rp-ca signing            			'IDA Sample RP Signing Dev'
-createLeaf stub_idp_signing_primary           		 ida-intermediary-ca    signing            			'IDA Stub IDP Signing Dev'
-createLeaf stub_country_signing_primary       		 ida-intermediary-ca    signing            			'IDA Stub Country Signing Dev'
-createLeaf hub_signing_secondary              		 idap-core-ca           signing            			'IDA Hub Signing Dev'
-createLeaf hub_encryption_secondary           		 idap-core-ca           encipherment       			'IDA Hub Encryption Dev'
-createLeaf sample_rp_encryption_secondary     		 ida-intermediary-rp-ca encipherment       			'IDA Sample RP Encryption Dev'
-createLeaf sample_rp_msa_encryption_secondary 		 ida-intermediary-rp-ca encipherment       			'IDA Sample RP MSA Encryption Dev'
-createLeaf sample_rp_msa_signing_secondary    		 ida-intermediary-rp-ca signing            			'IDA Sample RP MSA Signing Dev'
-createLeaf sample_rp_signing_secondary        		 ida-intermediary-rp-ca signing            			'IDA Sample RP Signing Dev'
-createLeaf stub_idp_signing_secondary         		 ida-intermediary-ca    signing            			'IDA Stub IDP Signing Dev'
-createLeaf stub_country_signing_secondary     		 ida-intermediary-ca    signing            			'IDA Stub Country Signing Dev'
-createLeaf stub_country_signing_tertiary      		 ida-intermediary-ca    signing_low_date   			'IDA Stub Country Signing Dev'
-createLeaf stub_country_signing_expired       		 ida-intermediary-ca    signing_expired    			'IDA Stub Country Signing Dev'
-createLeaf stub_country_signing_not_yet_valid      ida-intermediary-ca    signing_not_yet_valid   'IDA Stub Country Signing Dev'
-createLeaf expired_signing                    		 idap-core-ca           signing_expired    			'Expired Signing Dev'
+createLeaf verify-metadata_signing_a                 		 verify-metadata-ca        signing            			'GOV.UK Verify Test Metadata Signing A'
+createLeaf verify-metadata_signing_b                 		 verify-metadata-ca        signing            			'GOV.UK Verify Test Metadata Signing B'
+createLeaf verify-hub_signing_primary                		 verify-core-ca           signing            			'GOV.UK Verify Test Hub Signing'
+createLeaf verify-hub_encryption_primary             		 verify-core-ca           encipherment       			'GOV.UK Verify Test Hub Encryption'
+createLeaf verify-hub_connector_signing_primary      		 verify-core-ca           signing            			'GOV.UK Verify Test Connector Hub Signing'
+createLeaf verify-hub_connector_encryption_primary   		 verify-core-ca           encipherment       			'GOV.UK Verify Test Connector Hub Encryption'
+createLeaf verify-sample_rp_encryption_primary       		 verify-intermediary-rp-ca encipherment       			'GOV.UK Verify Test Sample RP Encryption'
+createLeaf verify-sample_rp_msa_encryption_primary   		 verify-intermediary-rp-ca encipherment       			'GOV.UK Verify Test Sample RP MSA Encryption'
+createLeaf verify-sample_rp_msa_signing_primary      		 verify-intermediary-rp-ca signing            			'GOV.UK Verify Test Sample RP MSA Signing'
+createLeaf verify-sample_rp_signing_primary          		 verify-intermediary-rp-ca signing            			'GOV.UK Verify Test Sample RP Signing'
+createLeaf verify-stub_idp_signing_primary           		 verify-intermediary-ca    signing            			'GOV.UK Verify Test Stub IDP Signing'
+createLeaf verify-stub_country_signing_primary       		 verify-intermediary-ca    signing            			'GOV.UK Verify Test Stub Country Signing'
+createLeaf verify-hub_signing_secondary              		 verify-core-ca           signing            			'GOV.UK Verify Test Hub Signing'
+createLeaf verify-hub_encryption_secondary           		 verify-core-ca           encipherment       			'GOV.UK Verify Test Hub Encryption'
+createLeaf verify-sample_rp_encryption_secondary     		 verify-intermediary-rp-ca encipherment       			'GOV.UK Verify Test Sample RP Encryption'
+createLeaf verify-sample_rp_msa_encryption_secondary 		 verify-intermediary-rp-ca encipherment       			'GOV.UK Verify Test Sample RP MSA Encryption'
+createLeaf verify-sample_rp_msa_signing_secondary    		 verify-intermediary-rp-ca signing            			'GOV.UK Verify Test Sample RP MSA Signing'
+createLeaf verify-sample_rp_signing_secondary        		 verify-intermediary-rp-ca signing            			'GOV.UK Verify Test Sample RP Signing'
+createLeaf verify-stub_idp_signing_secondary         		 verify-intermediary-ca    signing            			'GOV.UK Verify Test Stub IDP Signing'
+createLeaf verify-stub_country_signing_secondary     		 verify-intermediary-ca    signing            			'GOV.UK Verify Test Stub Country Signing'
+createLeaf verify-stub_country_signing_tertiary      		 verify-intermediary-ca    signing_low_date   			'GOV.UK Verify Test Stub Country Signing'
+createLeaf verify-stub_country_signing_expired       		 verify-intermediary-ca    signing_expired    			'GOV.UK Verify Test Stub Country Signing'
+createLeaf verify-stub_country_signing_not_yet_valid      verify-intermediary-ca    signing_not_yet_valid   'GOV.UK Verify Test Stub Country Signing'
+createLeaf verify-expired_signing                    		 verify-core-ca           signing_expired    			'Expired Signing Test'
 
 # Convert all the keys to .pk8 files
 for file in *-key.pem
